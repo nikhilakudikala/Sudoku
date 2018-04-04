4563ca7aa8a92b41aee406de2168b91973e051a5
Nikhila Kudikala <nikhila@kudikala.com>
2018-04-04 16:48:02 -0400

    New Matrix test values

--- test/SudokuTest.java
+++ test/SudokuTest.java
@@ -33,6 +33,17 @@ class SudokuTest {
 	            {9,0,6,0,4,0,0,3,0},
 	            {0,0,0,3,0,9,5,0,8}
 	            };
+		int[][] matrixValues1={
+				{0,9,2,0,7,0,0,0,0},
+				{5,7,0,1,0,8,0,3,9},
+				{0,0,8,2,0,5,4,0,7},
+				{0,0,0,0,0,6,8,0,0},
+				{8,6,0,0,0,0,0,7,2},
+				{0,0,4,3,0,0,0,0,0},
+				{2,0,9,5,0,7,6,0,0},
+				{3,4,0,8,0,9,0,2,1},
+				{0,0,0,0,1,0,7,9,0}
+				};
 		int[][] expectedMatrixValues= {
 		{8,7,5,4,9,3,2,6,1},
         {1,3,2,6,8,7,4,9,5},
@@ -44,8 +55,20 @@ class SudokuTest {
         {9,5,6,8,4,2,1,3,7},
         {7,2,1,3,6,9,5,4,8}
         };
+		int[][] expectedMatrixValues1={
+				{4,9,2,6,7,3,1,8,5},
+				{5,7,6,1,4,8,2,3,9},
+				{1,3,8,2,9,5,4,6,7},
+				{9,5,3,7,2,6,8,1,4},
+				{8,6,1,9,5,4,3,7,2},
+				{7,2,4,3,8,1,9,5,6},
+				{2,1,9,5,3,7,6,4,8},
+				{3,4,7,8,6,9,5,2,1},
+				{6,8,5,4,1,2,7,9,3}
+				};
 		Sudoku sk=new Sudoku(matrixValues,booleanMatrix);
 		sk.repeatMethodsEmptyBlock();
 	    Assertions.assertArrayEquals(expectedMatrixValues,sk.returnMatrix(matrixValues));
+	    Assertions.assertArrayEquals(expectedMatrixValues1,sk.returnMatrix(matrixValues1));
 	}
 }
